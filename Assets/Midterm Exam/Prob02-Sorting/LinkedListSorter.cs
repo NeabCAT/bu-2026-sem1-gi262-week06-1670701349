31e144a874ca8b9f0121c2993fa95b63109f7dd7
NeabCAT <neabkub01@gmail.com>
2026-09-22 10:47:32 +0700

    Midterm

--- Assets/Midterm Exam/Prob02-Sorting/LinkedListSorter.cs
+++ Assets/Midterm Exam/Prob02-Sorting/LinkedListSorter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,7 +13,41 @@ namespace MidtermExam.Prob02
         /// <returns>LinkedList ที่ได้รับการเรียงลำดับจากน้อยไปมากแล้ว</returns>
         public LinkedList<int> SortAscending(LinkedList<int> list)
         {
+            LinkedList<int> sorted = new LinkedList<int>();
+
+
+           sorted.AddLast(42);
+            sorted.AddLast(10);
+            sorted.AddLast(20);
+            
+            sorted.AddLast(20);
+            sorted.AddLast(10);
+
+
+            for (int i = 0; i < 7; i++)
+            {
+                sorted.AddLast(7);
+            }
+
+
+
             // TODO: Implement sorting algorithm for LinkedList<int> (Ascending)
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (list.Count < minIndex)
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                int temp = minIndex;
+                minIndex = list.Count;
+
+
+            }
             return list;
         }
 
